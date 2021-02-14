7979407e01241632dc6484201bf294a9000c0b37
dimka9910 <dimka99-10@mail.ru>
2021-02-15 01:08:33 +0300

    demo rest

---
+++ JAVA/Spring/demoRestController/src/main/java/com/example/demoRestController/DemoRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoRestController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestControllerApplication.class, args);
+	}
+
+}
---
+++ JAVA/Spring/demoRestController/src/main/java/com/example/demoRestController/TaskController.java
@@ -0,0 +1,25 @@
+package com.example.demoRestController;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TaskController {
+
+    private List<Task> tasks = new ArrayList<>();
+
+    public TaskController() {
+    }
+
+    @PostMapping(path = "/tasks")
+    public void addTask(@RequestBody Task task){
+        tasks.add(task);
+    }
+
+    @GetMapping(path = "/tasks/{id}")
+    public Task getTask(@PathVariable int id){
+        return tasks.get(id - 1);
+    }
+}
