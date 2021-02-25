7af6fe85f61688b3272bff7df0329be88c78ad47
dimka <dimka99-10@mail.ru>
2021-02-26 01:25:29 +0300

    several tasks

---
+++ JAVA/SPBSTU PROJECTS/Phone Book/Phone Book/task/src/phonebook/Main.java
@@ -0,0 +1,43 @@
+package phonebook;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Fake.fakesearch();
+
+
+//        System.out.println("Hello World!");
+//
+//        String fileData = "D:\\dimka\\Documents\\EDUCATIONAL_PROJECTS\\JAVA\\SPBSTU PROJECTS\\directory.txt";
+//        String fileSearch = "D:\\dimka\\Documents\\EDUCATIONAL_PROJECTS\\JAVA\\SPBSTU PROJECTS\\find.txt";
+//
+//        File file1 = new File(fileData);
+//        File file2 = new File(fileSearch);
+//
+//
+//        try {
+//            Scanner scanner = new Scanner(file1);
+//            List<String> list1 = new ArrayList<>();
+//            List<String> list2 = new ArrayList<>();
+//            while (scanner.hasNext()){
+//                scanner.nextInt();
+//                list1.add(scanner.nextLine().trim());
+//            }
+//
+//            scanner = new Scanner(file2);
+//            while (scanner.hasNext()){
+//                list2.add(scanner.nextLine().trim());
+//            }
+//
+//            SearchingMethod.lineralSearch(list1, list2);
+//        } catch (Exception e){
+//            System.out.println("kik");
+//        }
+
+    }
+}
---
+++ JAVA/SPBSTU PROJECTS/Phone Book/Phone Book/task/src/phonebook/SearchingMethod.java
@@ -0,0 +1,51 @@
+package phonebook;
+
+import javax.sound.midi.Soundbank;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public class SearchingMethod {
+
+
+
+    public static void lineralSearch(List<String> list1, List<String> list2){
+        long time = System.currentTimeMillis();
+        try {
+            System.out.println("Start searching...");
+            int i = 0;
+            int j = 0;
+            for (var l2: list2){
+                j++;
+                for (var l1: list1){
+                    if (l2.equals(l1)){
+                        i++;
+                        break;
+                    }
+                }
+            }
+            time = System.currentTimeMillis() - time;
+            String timeout = time / 60000 + " min. " + (time % 60000) / 1000 + " sec. " + (time % 60000) % 1000 + " ms.";
+            System.out.println("Found " + i + " / " + j + " entries. Time taken: " + timeout);
+        } catch (Exception e){
+            return;
+        }
+    }
+
+    public static List<String> bubbleSort (List<String> list, boolean asc){
+        for (int i = 0; i < list.size(); i++){
+            for (int j = 0; j < list.size() - 1; j++){
+                if (list.get(j).compareTo(list.get(j + 1)) > 0){
+                    String tmp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, tmp);
+                }
+            }
+        }
+        return list;
+        //int step = (int)Math.sqrt(list.size());
+    }
+
+
+}
